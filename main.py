5b9e4941de0a0e96babf8cf4757410636f47beca
Monika Krzyżanowska <muzic194@gmail.com>
2019-03-27 23:07:25 +0100

    Deciphering code

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"  # English Alphabet
 def replace_letter(letter, n):
     """Function takes letter_1 and returns letter_2 which is n-times on the left in alphabet"""
 
-    index = ((alphabet.find(letter.lower())) + n) % len(
-        alphabet)  # find letter in aplphabet (alphabet is variable defined above function - but can be inside)
+    index = ((alphabet.find(letter.lower())) + n) % len(alphabet)  # find letter in alphabet (alphabet is variable defined above function - but can be inside)
     alphabet_upper = alphabet.upper()  # prepare variable before for loop to not make .upper() method in each iteration
 
     if letter in alphabet:  # for small letters
@@ -18,7 +17,7 @@ def replace_letter(letter, n):
     elif letter in alphabet_upper:  # for big letters
         letter = alphabet_upper[index]
 
-    else:  # for everything that is not a letter - whiteholders, numbers, !@#$%^&*()_+ etc.
+    else:  # for everything that is not a letter - white holders, numbers, !@#$%^&*()_+ etc.
         letter = letter  # does not replace for anything - stay the same
     return letter
 
@@ -34,7 +33,7 @@ def decode(string, frequent_letter):
     Function returns translation with the most frequent "frequent_letter"""
 
     results = []  # list with translations for each n i alphabet
-    frequencies = []  # list with frequencies of freuent letter in each translation
+    frequencies = []  # list with frequencies of frequent letter in each translation
 
     for n in range(len(alphabet)):
         results.append(code(string, n))
